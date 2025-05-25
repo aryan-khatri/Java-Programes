09440a0ee33ba0aff97514fc25ff5469345e5ba3
“aryan-khatri” <aryankhatik721@gmail.com>
2025-05-25 14:19:24 +0530

    update

---
+++ Basic.java
@@ -0,0 +1,6 @@
+public class Basic {
+    public static void main(String[] args) {
+        String name = "Aryan";
+        System.out.println(name.length());       
+    }    
+}
---
+++ Hello.java
@@ -0,0 +1,6 @@
+// Jay Shree Ram
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Jay Shree Ram");
+    }
+}
